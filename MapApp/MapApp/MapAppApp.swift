85aa26795e3ee62235b938403e655265d48aff17
Ostap <mamykinosk@gmail.com>
2026-04-09 21:30:16 +0300

    setup architecture

---
+++ MapApp/MapApp/MapAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MapAppApp.swift
+//  MapApp
+//
+//  Created by OstapMamykin on 24.03.2026.
+//
+
+import SwiftUI
+
+@main
+struct MapAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
